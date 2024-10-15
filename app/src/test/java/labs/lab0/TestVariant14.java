a8a2e0e2bd8915214a359b63ea3be17859ddf195
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-10-15 15:22:19 +0300

    Updated

--- app/src/test/java/labs/lab0/TestVariant14.java
+++ app/src/test/java/labs/lab0/TestVariant14.java
@@ -1,5 +1,28 @@
 package labs.lab0;
 
+// import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class TestVariant14 {
-  
+
+    @ParameterizedTest
+    @MethodSource
+    public void arrayTest(double[] array, int[] result) {
+        assertArrayEquals(result, new Variant14().arrayTask(array));
+    }
+
+    private static Stream<Arguments> arrayTest() {
+        return Stream.of(
+                Arguments.of(new double[]{2, 3, 6}, new int[]{0}),
+                Arguments.of(new double[]{12, 6, 3}, new int[]{0,1,2})
+        );
+    }
+
+
 }
